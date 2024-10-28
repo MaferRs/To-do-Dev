82ce414041e3c40e1a3d9a5e5e0164c90510db77
Maria Fernanda <90789503+MaferRs@users.noreply.github.com>
2024-10-28 16:37:53 -0300

    🚧 interface da tela de login criada

---
+++ todo-dev/src/pages/login/index.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { View, Image, Text, TextInput } from 'react-native';
+import { styles } from './styles';
+
+import imageLogin from '../../assets/login.png';
+import CustomInput from '../../components/input/custom-input';
+import CustomButton from '../../components/button/custom-button';
+
+export default function Login() {
+  const [email, setEmail] = useState<string>('');
+  const [senha, setSenha] = useState<string>('');
+
+  const handleLogin = () => {
+    console.log('Email:', email);
+    console.log('Senha:', senha);
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageLogin}>
+        <Image style={styles.image} source={imageLogin} />
+        <Text style={styles.loginText}>Login</Text>
+      </View>
+      <View style={styles.form}>
+        <CustomInput
+          value={email}
+          onChangeText={setEmail}
+          placeholder="E-mail"
+        />
+        <CustomInput
+          value={senha}
+          onChangeText={setSenha}
+          placeholder="Senha"
+          secureTextEntry
+        />
+      </View>
+      <View style={styles.button}>
+        <CustomButton>Entrar</CustomButton>
+      </View>
+      <View>
+        <Text>Não tem uma conta? Cadastre-se</Text>
+      </View>
+    </View>
+  );
+}
---
+++ todo-dev/src/pages/login/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    gap: 40,
+    height: '100%',
+    paddingVertical: 60,
+  },
+  imageLogin: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loginText: {
+    color: '#314D3A',
+    fontSize: 40,
+    fontWeight: '800',
+  },
+  image: { width: 200, height: 200 },
+  form: { width: '100%' },
+  button: {},
+});
