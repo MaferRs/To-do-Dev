a9f8043857f6722de9a460ef10e50d4d540ad64a
Maria Fernanda <90789503+MaferRs@users.noreply.github.com>
2024-10-28 16:37:31 -0300

    ✨ componentes

---
+++ todo-dev/src/components/button/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  button: {
+    width: 177,
+    height: 52,
+    borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: '#314D3A',
+    fontSize: 16,
+    fontWeight: '800',
+  },
+});
---
+++ todo-dev/src/components/input/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    marginBottom: 16,
+  },
+  label: {
+    fontSize: 16,
+    marginBottom: 8,
+  },
+  input: {
+    width: 350,
+    height: 48,
+    backgroundColor: '#EDEDED',
+    borderRadius: 20,
+    paddingLeft: 20,
+  },
+});
