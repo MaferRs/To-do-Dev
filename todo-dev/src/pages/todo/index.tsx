ec590fe22fca2d86999e799116f4019483e853c5
Maria Fernanda <90789503+MaferRs@users.noreply.github.com>
2024-10-29 22:12:03 -0300

    🚧 toDo em cosntrução

--- todo-dev/src/pages/todo/index.tsx
+++ todo-dev/src/pages/todo/index.tsx
@@ -1,10 +1,24 @@
-import { View, Text } from 'react-native';
+import { View, Text, KeyboardAvoidingView, ScrollView } from 'react-native';
 import { styles } from './styles';
 
 export default function Todo() {
   return (
-    <View style={styles.container}>
-      <Text>KATCHAU</Text>
-    </View>
+    <KeyboardAvoidingView behavior="height" style={{ flex: 1 }}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Bem Vinda, Ana F.</Text>
+      </View>
+
+      <ScrollView
+        contentContainerStyle={styles.containerTodo}
+        keyboardShouldPersistTaps="handled"
+      >
+        <View style={styles.contentTodo}>
+          <Text>KATCHAU</Text>
+        </View>
+      </ScrollView>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>Footer</Text>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
--- todo-dev/src/pages/todo/styles.ts
+++ todo-dev/src/pages/todo/styles.ts
@@ -1,11 +1,47 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-  container: {
+  containerTodo: {
+    flex: 1,
+  },
+  contentTodo: {},
+  header: {
+    width: '100%',
+    height: 150,
+    paddingVertical: 20,
+    backgroundColor: '#ADCEB7',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mainContentContainer: {
+    flexGrow: 1,
+    padding: 20,
+  },
+  main: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 20,
-    gap: 30,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  footer: {
+    width: '100%',
+    height: 80,
+    paddingVertical: 20,
+    backgroundColor: '#f8f8f8',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  footerText: {
+    fontSize: 18,
+    backgroundColor: '#ADCEB7',
   },
 });
